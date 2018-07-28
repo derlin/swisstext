785a1c4a3132df8139f18690af7bee6aa17bb03a
Lucy Linder <lucy.derlin@gmail.com>
2018-07-28 11:39:03 +0200

    change error messages

--- blueprints/errorhandlers.py
+++ blueprints/errorhandlers.py
@@ -1,5 +1,6 @@
 import traceback
 
+import mongoengine
 from flask.blueprints import Blueprint
 
 from utils import responses
@@ -20,8 +21,6 @@ def handle_app_errors(error):
         cause=error.cause,
         message=error.message)
 
-
-
 @errorhandlers.app_errorhandler(IOError)
 def handle_db_errors(error):
     _on_error()
@@ -37,18 +36,15 @@ def handle_db_errors(error):
 @errorhandlers.app_errorhandler(Exception)
 def handle_unknown_errors(error):
     _on_error()
+    typ= type(error)
     return responses.error(
         status_code=500,
-        cause='Unknown Error',
+        cause="%s: %s" % (typ.__module__, typ.__name__),
         message=str(error))
 
 
 def _on_error():
-    # HIGHLY IMPORTANT: rollback the database transaction to undo all the queries done
-    # during this session before the error occurred.
-    # This must be done manually because once an error has been handled (here by our custom handlers),
-    # it is not propagated further, so app.teardown_XX(exception) receives a None argument --> no way to detect
-    # something went wrong !
-    #db_abort()
+    # put any logic that should run whatever the error, for example
+    # a database rollback.
     # log the error, limiting the stacktrace to 5 calls
     logger.error(traceback.format_exc(limit=5, chain=False))
