83fec75a8ba03f1a258baabd98144b8220f9baee
Lucy Linder <lucy.derlin@gmail.com>
2019-05-22 07:59:18 +0200

    simplify config

--- backend/swisstext/cmd/scraping/tools/__init__.py
+++ backend/swisstext/cmd/scraping/tools/__init__.py
@@ -9,3 +9,20 @@ This package contains various implementations of the different pipeline tools.
     :py:mod:`~swisstext.cmd.scraping.config`
         The default configuration instantiates tools from this package
 """
+
+# deciders
+from .basic_decider import BasicDecider
+# seed creators
+from .basic_seed_creator import BasicSeedCreator, IdfSeedCreator
+# crawlers
+from .bs_crawler import BsCrawler, CleverBsCrawler
+# splitters
+from .moses_splitter import MosesSplitter
+from .punkt_splitter import PunktSplitter
+# sentence filters
+from .pattern_sentence_filter import PatternSentenceFilter
+# savers
+from .console_saver import ConsoleSaver
+from .mongo_saver import MongoSaver
+# language id
+from .swigspot_langid import SwigspotLangid
