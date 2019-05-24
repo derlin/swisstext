9b3b64693cd3e6629648069c9e3ed7fdee6052df
Lucy Linder <lucy.derlin@gmail.com>
2019-05-24 11:17:19 +0200

    import tools in __init__.py

--- backend/swisstext/cmd/searching/tools/__init__.py
+++ backend/swisstext/cmd/searching/tools/__init__.py
@@ -9,3 +9,9 @@ This package contains various implementations of the different search engine too
     :py:mod:`~swisstext.cmd.searching.config`
         The default configuration instantiates tools from this package
 """
+
+from .google_search import GoogleGeneratorFactory
+from .start_page import StartPageGeneratorFactory
+
+from .console_saver import ConsoleSaver
+from .mongo_saver import MongoSaver
