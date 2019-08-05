87b6361034aa21f746f4ba43c970753899cf8cb6
Lucy Linder <lucy.derlin@gmail.com>
2019-08-05 09:46:08 +0200

    stop looking at parent url, just enforce at least one new sentence to crawl children

--- extra/one_new_decider.py
+++ extra/one_new_decider.py
@@ -6,8 +6,5 @@ class OneNewDecider(BasicDecider):
 
     def should_children_be_crawled(self, page: Page) -> bool:
         if super().should_children_be_crawled(page):
-            # if parent_url is None it means the url comes from
-            # either a file or a seed, thus it might be interesting to
-            # go further anyway. But in case we already have a crawl depth > 1,
             # just don't try going further if no new sg is found
-            return page.parent_url is None or len(page.new_sg) > 0
+            return len(page.new_sg) > 0
