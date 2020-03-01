acdf9216e56c97af7576f209007af73b17a2ac35
Lucy Linder <lucy.derlin@gmail.com>
2020-03-01 10:31:06 +0100

    update license + use single quotes

--- src/backend/setup.py
+++ src/backend/setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-# with open("README.md", "r") as fh:
+# with open('README.md', 'r') as fh:
 #     long_description = fh.read()
 
 
 setuptools.setup(
-    name="swisstext_cmd",
-    version="0.0.1",
-    author="Lucy Linder",
-    author_email="lucy.derlin@gmail.com",
-    description="SwissText: Swiss German Corpus Generator",
-    license='Apache License 2.0',
-    long_description="TODO",
-    url="https://github.com/derlin/swisstext",
+    name='swisstext_cmd',
+    version='0.0.1',
+    author='Lucy Linder',
+    author_email='lucy.derlin@gmail.com',
+    description='SwissText: Swiss German Corpus Generator',
+    license='CC BY-NC 4.0',
+    long_description='TODO',
+    url='https://github.com/derlin/swisstext',
 
     packages=setuptools.find_packages(),
     package_data={'': ['*.yaml', '*.pickle', '*.txt']},  # include yaml and pickle from any module
@@ -23,25 +23,25 @@ setuptools.setup(
         ]
     },
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: Creative Commons (CC BY-NC 4.0)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: Creative Commons (CC BY-NC 4.0)',
+        'Operating System :: OS Independent',
     ),
     install_requires=[
-        "swisstext_mongo",  # TODO
-        "beautifulsoup4>=4.6.0",
-        "requests>=2.20.0",
-        "bs4>=0.0.1",
-        "pyyaml>=5.1",
-        "cityhash>=0.2.3.post9",
-        "nltk>=3.3",
-        "pytimeparse>=1.1.8",
-        "scikit-learn>=0.19.1",
-        "regex",
-        "click>=6.7",
-        "jusText==2.2.0",
-        "ftfy",
-        "get-html"
+        'swisstext_mongo',  # TODO
+        'beautifulsoup4>=4.6.0',
+        'requests>=2.20.0',
+        'bs4>=0.0.1',
+        'pyyaml>=5.1',
+        'cityhash>=0.2.3.post9',
+        'nltk>=3.3',
+        'pytimeparse>=1.1.8',
+        'scikit-learn>=0.19.1',
+        'regex',
+        'click>=6.7',
+        'jusText==2.2.0',
+        'ftfy',
+        'get-html'
     ],
     tests_require=[
         'pytest',
--- src/mongo/setup.py
+++ src/mongo/setup.py
@@ -1,28 +1,28 @@
 import setuptools
 from os import path
 
-with open(path.join(path.dirname(path.realpath(__file__)), 'README.md'), "r") as fh:
+with open(path.join(path.dirname(path.realpath(__file__)), 'README.md'), 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='swisstext_mongo',
     version='0.0.1',
-    author="Lucy Linder",
-    author_email="lucy.derlin@gmail.com",
-    license='Apache License 2.0',
-    description="SwissText common MongoEngine schemas",
+    author='Lucy Linder',
+    author_email='lucy.derlin@gmail.com',
+    license='CC BY-NC 4.0',
+    description='SwissText common MongoEngine schemas',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/derlin/swisstext",
+    long_description_content_type='text/markdown',
+    url='https://github.com/derlin/swisstext',
 
     packages=setuptools.find_packages(),
 
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: Creative Commons (CC BY-NC 4.0)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: Creative Commons (CC BY-NC 4.0)',
+        'Operating System :: OS Independent',
     ),
     install_requires=[
-        "mongoengine<0.19,>=0.18",
+        'mongoengine<0.19,>=0.18',
     ],
 )
